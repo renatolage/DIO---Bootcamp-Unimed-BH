9ebf861044b3b8a66d9c1a4315a7e7278e163c5b
renatolage <renato.lage85@gmail.com>
2022-05-27 13:58:27 -0300

    Inclusão do arquivo index.php com o desafio de accordion

---
+++ index.php
@@ -0,0 +1,51 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DIO - Unimed-BH - Desafio Accordion</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+
+<div id="accordion">
+  <h3>Modulo I - Princípio de Desenvolvimento de Software</h3>
+  <div>
+    <p>
+Intrudução à Programação e Pensamento Computacional</br>
+Intrudução ao Git e GitHub</br>
+Criando seu Primeiro Projeto GitHub para compartilhar seu Progresso</br>
+    </p>
+  </div>
+  <h3>Modulo II - Fundamentos Web com HTML e CSS</h3>
+  <div>
+    <p>
+Primeiros Passo Para Desenvolvimento WEB</br>
+Intrudução à criação de Wbsites com HTML5 e CSS3</br>
+Posicionando Elementos com FlexBox em CSS</br>
+Recriando a página inicial do Instagram</br>
+    </p>
+  </div>
+  <h3>Modulo III - Conhecendo a Linguagem de Programação JavaScript</h3>
+  <div>
+    <p>
+Introdução ao Javascript</br>
+Sintaxe e Operadores</br>
+Sintaxe básica em JavaScipt</br>
+Variáveis e Tipos</br>
+Trabalhando co Módulos em JavaScript</br>
+Funções</br>
+  
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
