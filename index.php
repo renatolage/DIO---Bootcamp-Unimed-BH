4a51e439755ba84594ad0a0aae92171d2edc599b
renatolage <renato.lage85@gmail.com>
2022-05-29 19:58:54 -0300

    Atualização do arquivo index.php, com adição de estilos em css.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>DIO - Unimed-BH - Desafio Accordion</title>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
   <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
@@ -12,36 +12,45 @@
     $( "#accordion" ).accordion();
   } );
   </script>
+  <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-
-<div id="accordion">
-  <h3>Modulo I - Princípio de Desenvolvimento de Software</h3>
+<h1 class="subtitle"> DIO - Bootcamp Unimed-BH - Desafios </h1>
+<img src="imagens/estudar.jpg" alt="Pessoa estudando" class="body-image">
+<div id="accordion" class="div-accordion">
+  <h3 class="subtitle">Modulo I - Princípio de Desenvolvimento de Software</h3>
   <div>
     <p>
-Intrudução à Programação e Pensamento Computacional</br>
-Intrudução ao Git e GitHub</br>
-Criando seu Primeiro Projeto GitHub para compartilhar seu Progresso</br>
+    <ul>
+     <li>Intrudução à Programação e Pensamento Computacional</li>
+     <li>Intrudução ao Git e GitHub</li>
+     <li>Criando seu Primeiro Projeto GitHub para compartilhar seu Progresso</li>
+     </ul>
     </p>
   </div>
-  <h3>Modulo II - Fundamentos Web com HTML e CSS</h3>
+  <h3 class="subtitle">Modulo II - Fundamentos Web com HTML e CSS</h3>
   <div>
     <p>
-Primeiros Passo Para Desenvolvimento WEB</br>
-Intrudução à criação de Wbsites com HTML5 e CSS3</br>
-Posicionando Elementos com FlexBox em CSS</br>
-Recriando a página inicial do Instagram</br>
+    <ul>
+    <li>Primeiros Passo Para Desenvolvimento WEB</li>
+    <li>Intrudução à criação de Wbsites com HTML5 e CSS3</li>
+    <li>Posicionando Elementos com FlexBox em CSS</li>
+    <li>Recriando a página inicial do Instagram</li
+    </ul>
     </p>
   </div>
-  <h3>Modulo III - Conhecendo a Linguagem de Programação JavaScript</h3>
+  <h3 class="subtitle">Modulo III - Conhecendo a Linguagem de Programação JavaScript</h3>
   <div>
     <p>
-Introdução ao Javascript</br>
-Sintaxe e Operadores</br>
-Sintaxe básica em JavaScipt</br>
-Variáveis e Tipos</br>
-Trabalhando co Módulos em JavaScript</br>
-Funções</br>
+     <ul>
+     <li>Introdução ao Javascript</li>
+     <li>Sintaxe e Operadores</li>
+     <li>Sintaxe básica em JavaScipt</li>
+     <li>Variáveis e Tipos</li>
+     <li>Trabalhando co Módulos em JavaScript</li>
+     <li>Funções</li>
+     </ul>
+    </p>
   
   </div>
 </div>
